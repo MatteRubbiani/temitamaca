63646a5dc9c4c00240f226aa96b014d7f1e8860e
Matteo Rubbiani <Matteo@iMac-di-Maria.home-life.hub>
2019-04-30 22:06:08 +0200

    f

--- Resources/GetProblemiSimulazione.py
+++ Resources/GetProblemiSimulazione.py
@@ -8,6 +8,30 @@ import time
 
 class GetProblemiSimulazione(Resource):
 
+    def find_valore_effettivo(problema, simulazione, problema_riferimento):
+        jolly=1
+        if problema.jolly:
+            jolly=2
+        if problema.risolto:
+            tempo=problema.tempo_risoluzione
+        else:
+            tempo=int(time.time())
+        tempo_trascorso = tempo-simulazione.inizio
+        bonus = tempo_trascorso//60
+        valore_puro = problema_riferimento.valore + bonus
+        return valore_puro*jolly
+
+    def find_valore(problema, simulazione, problema_riferimento, gara):
+        jolly=1
+        if problema.jolly:
+            jolly=2
+        errore=problema.errori*jolly*gara.errore
+        if not problema.risolto:
+            return  -errore
+
+        valore_effettivo = find_valore_effettivo(problema, simulazione, problema_riferimento)
+        totale = valore_effettivo-errore
+
     def get (self):
         sim=request.args.get('simulazione')
         a=CheckStatus(sim)
@@ -64,41 +88,26 @@ class GetProblemiSimulazione(Resource):
 
         if a==2:
             simulazione=SimulazioneModel.find_by_nome(sim)
+            gara=GaraModel.find_by_id(simulazione.gara_id)
+
             problemi1=ProblemiSimulazioneModel.find_by_simulazione_id(simulazione.id)
 
             if problemi1 is None:
                 return "non ci sono problemi in questa simulazione"
             d=[]
-
             for i in problemi1:
                 d.append(i)
             f = sorted(d, key=lambda x: x.numero)
 
-            #problemi=d.sort(key=lambda x: ProblemaModel.find_id(x.problema_id).numero)
-            erroreGara=GaraModel.find_by_id(simulazione.gara_id).errore
             array=[]
             for i in f:
-                valore=ProblemaModel.find_by_id(i.problema_id).valore
-                if i.risolto:
-                    errori=i.tentativi-1
-                    totalScore=valore-(errori*erroreGara)
-                    if i.jolly:
-                        totalScore=totalScore*2
-
-                else:
-                    errori=i.tentativi
-                    totalScore=-(errori*erroreGara)
-                    if i.jolly:
-                        totalScore=totalScore*2
-                pb=ProblemaModel.find_by_id(i.problema_id)
-                if i.jolly:
-                    val=pb.valore*2
-                else:
-                    val=pb.valore
+                problema_riferimento=ProblemaModel.find_by_id(i.problema_id)
+                valoreEffettivo=find_valore_effettivo(i, simulazione, problema_riferimento)
+                valore=find_valore(i, simulazione, problema_riferimento, gara)
                 array.append({"risolto":i.risolto,
-                            "valore":totalScore,
+                            "valore":valore,
                             "errori":errori,
-                            "valoreEffettivo":val,
+                            "valoreEffettivo":valoreEffettivo,#valore che avrebbe senza gli errori
                             "jolly":i.jolly})
             return array, 202
 
